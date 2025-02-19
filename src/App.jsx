04eb9a1daaf283cf646400ffc4dcebfcb8a4629c
fireball90 <58610433+fireball90@users.noreply.github.com>
2025-02-19 10:45:16 +0100

    endpoint bekotes, hibakezeles alapja

--- src/App.jsx
+++ src/App.jsx
@@ -8,51 +8,94 @@ import './App.css'
 
 function App() {
 
+    const endpoint = import.meta.env.VITE_API_ENDPOINT;
+
     const [modal, setModal] = useState('loading'); // 'loading', 'error', 'form', 'success'
     const [data, setData] = useState(null);
+    const [hash, setHash] = useState(null);
     const [loadingText, setLoadingText] = useState('Adatok betöltése folyamatban') // 'Adatok elküldése folyamatban'
     const [loadingNumber, setLoadingNumber] = useState('4') // form: 4, error: 2, success: 1
+    const [showAlert, setShowAlert] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
-            try {
+            try{
                 const urlParams = new URLSearchParams(window.location.search);
-                const hash = urlParams.get('hash');
+                const token = urlParams.get('token');
+                setHash(token);
 
-                if (!hash || hash.length !== 12) {
-                    throw new Error('Invalid hash');
+                if (!token){
+                    throw new Error('Invalid token');
                 }
 
-                const response = await fetch(`https://example.com/api/?hash=${hash}`);
+                const response = await fetch(`${endpoint}/backend/api/alarms`,{
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ token })
+                });
+
                 const result = await response.json();
 
-                if (!result || Object.keys(result).length === 0) {
+                if (!result || Object.keys(result).length === 0){
+                    setErrorMsg('Nem sikerült csatlakozni a szerverhez. Kérem próbálja újra később vagy ellenőrizze a link végén található kódot.')
+                    setShowAlert('true');
                     throw new Error('No data found');
                 }
 
-                setData(result);
+                //console.table(result.activeAlarms);
+                setData(result.activeAlarms);
+
                 setModal('form');
-            } catch (error) {
-                setModal('form'); //error kell majd ide
             }
-        };
+            catch(error){
+                setModal('error');
+            }
+        }
 
         fetchData();
     }, []);
 
+    useEffect(() => {
+        if (showAlert) {
+            const timer = setTimeout(() => {
+                setShowAlert(false);
+            }, 8000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [showAlert]);
+
     const handleSubmit = async (formData) => {
         setModal('loading');
+        //console.table(formData);
+
+        if (!formData.password){
+            setErrorMsg('A jelszó megadása kötelező!')
+            setShowAlert('true');
+            setModal('form');
+            return;
+        }
+
         try {
-            const response = await fetch('https://example.com/api/submit', {
+            const response = await fetch(`${endpoint}/backend/api/cancel-alarm`, {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify(formData),
             });
+
+            //console.log(response)
 
             if (response.status === 200) {
                 setModal('success');
             } else {
-                setModal('error');
+                setErrorMsg(response.statusText || "Hibás adatok. Kérjük ellenőrizze a megadott jelszót.")
+                setShowAlert('true');
+                setModal('form');
             }
         } catch (error) {
             setModal('error');
@@ -61,6 +104,11 @@ function App() {
 
   return (
     <>
+    {showAlert && (
+        <div className="alert alert-danger toprow" role="alert">
+           {errorMsg}
+        </div>
+    )}
     <div className='outer'>
       <div className='d-flex align-items-center justify-content-center'>
         <a href="https://www.multialarm.hu/" target="_blank">
@@ -70,7 +118,7 @@ function App() {
       <div>
             {modal === 'loading' && <Loading text={loadingText} number={loadingNumber}/>}
             {modal === 'error' && <Error />}
-            {modal === 'form' && <Dataform data={data || {}} onSubmit={handleSubmit} />}
+            {modal === 'form' && <Dataform data={data || {}} onSubmit={handleSubmit} hash={hash} />}
             {modal === 'success' && <Success />}
         </div>
     </div>
--- src/components/Dataform/Dataform.jsx
+++ src/components/Dataform/Dataform.jsx
@@ -1,15 +1,26 @@
 import { useState, useEffect } from 'react';
 import style from './dataform.module.css';
-import 'react-datepicker/dist/react-datepicker.css';
-import './CustomDatePicker.css';
 
-export default function Dataform({ data = {}, onSubmit }) {
+export default function Dataform({ data = {}, onSubmit , hash}) {
+    const [selectedAddress, setSelectedAddress] = useState('');
 
     const [formData, setFormData] = useState({
-        phonenumber: data.phone || '',
-        address: data.address || '',
-        password: data.password || ''
+        token: hash,
+        intAccId: '',
+        personId: '',
+        password: '',
     });
+
+    useEffect(() => {
+        if (Array.isArray(data) && data.length > 0) {
+            setFormData((prevState) => ({
+                ...prevState,
+                intAccId: data[0].int_acc_id || '',
+                personId: data[0].person_id || '',
+            }));
+            setSelectedAddress(data[0].address || '');
+        }
+    }, [data]);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -28,14 +39,14 @@ export default function Dataform({ data = {}, onSubmit }) {
                         <h3>Riasztás lemondása</h3>
                         <select name="language" id="language" className={style.languagechange}>
                             <option value="hu">HU</option>
-                            <option value="en">EN</option>
-                            <option value="ger">GER</option>
+{/*                             <option value="en">EN</option>
+                            <option value="ger">GER</option> */}
                         </select>
                     </div>
 
                     <form onSubmit={(e) => { e.preventDefault(); onSubmit(formData); }}>
 
-                        <div className={style.responsive}>
+                        {/* <div className={style.responsive}>
                             <label htmlFor='phone'>Bejelentkező telefonszáma</label>
                             <div className={style.iconphone} />
                             <input
@@ -46,19 +57,19 @@ export default function Dataform({ data = {}, onSubmit }) {
                                 placeholder={data.phone || "+36 30 555 4444"}
                                 onChange={handleChange}
                             />
-                        </div>
+                        </div> */}
                         <div className={style.responsive}>
                             <label htmlFor='address'>Kérjük, válassza ki az érintett ingatlant</label>
                             <div className={style.iconaddress} />
-                            <select name="address" id="address" className={style.addressSelect} onChange={handleChange}>
-                                {Array.isArray(formData.address) && formData.address.length > 0 ? (
-                                    formData.address.map((address, index) => (
-                                        <option key={index} value={address.value}>
-                                            {address.text}
+                            <select name="address" id="address" className={style.addressSelect} value={selectedAddress} onChange={(e) => setSelectedAddress(e.target.value)}>
+                                {Array.isArray(data) && data.length > 0 ? (
+                                    data.map((item, index) => (
+                                        <option key={index} value={item.address}>
+                                            {item.address}
                                         </option>
                                     ))
                                 ) : (
-                                    <option value=""></option>
+                                    <option value="">Nincs cím</option>
                                 )}
                             </select>
                         </div>
